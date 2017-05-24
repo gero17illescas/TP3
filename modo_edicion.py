ee775fd57be11bb8787e9281b9181d40a3e5d19b
gero17illescas <gero17illescas@gmail.com>
2017-05-24 16:17:08 -0300

    Cambios
    Agregue un archivo tda con lo basico de listas enlazadas simples, pilas y colas (lo que hicimos en clase)
    "intente" desarrollarlo un poco, cree una clase 'editor' que tiene toda la info
    los marks los represente en la timeline
    los tracks en track
    cada mark tiene un diccionario, donde una de sus claves y valor es la duracion de esa mark

--- modo_edicion.py
+++ modo_edicion.py
@@ -1,126 +1,115 @@
 import cmd
+import soundPlayer
+import tda
+class Editor():
+	def __init__(self):
+		self.timeline = tda.ListaEnlazada()
+		self.cursor = None
+		self.pila = tda.Pila()
+		self.tracks = tda.ListaEnlazada()
+		self.sound = {"sine": soundPlayer.SoundFactory.get_sine_sound,
+			 "squa": soundPlayer.SoundFactory.get_square_sound,
+			 "sile": soundPlayer.SoundFactory.get_silence_sound,
+			 "tria": soundPlayer.SoundFactory.get_triangular_sound,
+			 "nois": soundPlayer.SoundFactory.get_noise_sound,}
+	def avanzar(self, N):
+		N = int(N)
+		cont = 0
+		self.cursor = self.timeline.prim
+		while cont < N:
+			self.cursor = self.timeline.prim.prox
+			self.pila.apilar(self.cursor)
+	def retroceder(self, N):
+		if not self.pila.esta_vacia():
+			for _ in range (N):
+				self.cursor = self.pila.desapilar()
+editor = Editor()
 class Shell(cmd.Cmd):
 	intro = "Bienvenido a mi programa.\n Ingrese help o ? para listar los comandos.\n"
 	prompt = "*>>"
-	def do_COMANDO(self,parametros):
-		"""Este metodo ejecuta un comando"""
-	print("COMANDO - Parametros: ",parametros)
 	def do_LOAD (self,file):
-		"""Carga la canción desde el archivo. Reemplaza la canción en edición actual si es que la hay."""
+		"""Carga la cancion desde el archivo. Reemplaza la cancion en edicion actual si es que la hay."""
 		# Ejemplo :
 		# LOAD last surprise.plp
-		# Carga al modo de edición la canción “Last Surprise”. El cursor del
+		# Carga al modo de edicion la cancion “Last Surprise”. El cursor del
 		# editor queda al inicio de la misma. Si no existe el archivo, se le debe
 		# informar al usuario.
 	def do_STORE (self,file):
-		"""Guarda la canción."""
+		"""Guarda la cancion."""
 		# Ejemplo :
 		# 	STORE shadow world.plp
 		# 	Guarda la cancion en edicion actual con el nombre indicado.
 
 	def do_STEP(self):
 		"""Avanza a la siguiente marca de tiempo.Si no hay mas marcas hacia adelante, no hace nada."""
-		# Ejemplo :
-		# 	STEP
-		# 	Eso avanza hacia la siguiente marca de tiempo. 
-
+		do_STEPM(1)
 	def do_BACK(self):
-		"""Retrocede a la anterior marca de tiempo.Si no hay mas marcas hacia atrás, no hace nada."""
-		# Ejemplo :
-		# BACK
-		# Eso retrocede hacia la anterior marca de tiempo. 
-
+		"""Retrocede a la anterior marca de tiempo.Si no hay mas marcas hacia atras, no hace nada."""
+		do_BACKM(1)
 	def do_STEPM (self, N):
 		"""Avanza N marcas de tiempo hacia adelante. Si no hay mas marcas hacia adelante, no hace nada."""
-		# Ejemplo :
-		# 	STEPM 1
-		# 	Eso avanza hacia la siguiente marca de tiempo.  Ídem que STEP.
-		# Ejemplo :
-		# 	STEPM 6
-		# 	Eso avanza 6 marcas de tiempo hacia adelante. Si no hay suficientes
-		# 	marcas hacia adelante, se queda al final.
-
+		
+		editor.avanzar(int(N))
 	def do_BACKM (self, N):
-		"""Retrocede N marcas de tiempo hacia atrás. Si no hay mas marcas hacia adelante, no hace nada."""
-		# Ejemplo :
-		# 	BACKM 1
-		# 	Eso retrocede hacia la anterior marca de tiempo. Si no hay mas marcas
-		# 	hacia atrás, no hace nada. Ídem que BACK
-		# Ejemplo :
-		# 	BACKM 5
-		# 	Eso retrocede 6 marcas de tiempo. Si no suficientes marcas hacia atrás,
-		# 	se queda al inicio.
-
+		"""Retrocede N marcas de tiempo hacia atras. Si no hay mas marcas hacia atras, no hace nada."""
+		editor.retroceder(int(N))
 	def do_TRACKADD (self, funcion, frecuencia, volumen=100):
-		'''Agrega un track con el sonido indicado.'''
-		# Ejemplo :
-		# 	Suponiendo que se tiene la función sine dentro de las que ofrece el
-		# 	programa:
-		# 	TRACKADD sine 440 0.2
-		# 	Eso agrega un track con un sonido generado por la función sine, a 440hz
-		# 	y con un volumen del 20 %.
-
-	def do_TRACKDEL(self, track):
-		"""Elimina un track por numero."""
-		# Ejemplo :
-		# 	Suponiendo que se tienen 3 tracks:
-		# 	TRACKDEL 2
-		# 	TRACKDEL 2
-		# 	Eso elimina primero el track en la posición 2, y luego elimina el track
-		# 	que originalmente era el track 3, pero que luego del borrado ocupa la
-		# 	posición 2.
-
+		"""Agrega un track con el sonido indicado.\n
+			-> funcion (str): tipo de sonido\n
+			-> frecuencia(int)\n
+			-> volumen(int)\n"""
+		editor.tracks.append(editor.sound.get(funcion)(frecuencia,volumen))
+	def do_TRACKDEL(self, posicion):
+		"""Elimina un track por numero.Si no se especifica uno elimina\n
+		el ultimo tarck.\n
+			-> posicion(int)"""
+		tracks.pop(posicion)
 	def do_MARKADD(self,duration):
-		"""Agrega una marca de tiempo de la duración establecida. Originalmente
-		todos los tracks arrancan como deshabilitados."""
-		# Ejemplo :
-		# 	MARKADD 2
-		# 	Agrega una marca de tiempo de 2 segundos en la posición actual del
-		# 	cursor.
-
-	def do_MARKADDNEXT(self,duration):
-		"""Igual que MARKADD pero la inserta luego de la marca en la cual esta
-		actualmente el cursor."""
+		"""Agrega una marca de tiempo de la duracion establecida.\n
+		Originalmente todos los tracks arrancan como deshabilitados."""
+		editor.timeline.append({"duracion":duration})
+	def do_MARKADDNEXT(self, duration):
+		
+		"""Igual que MARKADD pero la inserta luego de la marca en la \n
+		cual esta actualmente el cursor."""
 		
 	def do_MARKADDPREV(self,duration):
-		"""Igual que MARKADD pero la inserta antes de la marca en la cual esta
-		actualmente el cursor."""
+		"""Igual que MARKADD pero la inserta antes de la marca en la \n
+		cual esta actualmente el cursor."""
 
 	def do_TRACKON(self, track):
-		"""Habilita al track durante la marca de tiempo en la cual esta parada el
-		cursor."""
-		# Ejemplo :
-		# 	Suponiendo que se tienen 3 tracks y la marca fue recién creada:
-		# 	TRACKON 2
-		# 	TRACKON 3
-		# 	Habilita los tracks 2 y 3 de la marca. Cuando se reproduzca la misma,
-		# 	se reproducirán los sonidos de los tracks 2 y 3 durante la duración de
-		# 	la misma, pero no el del track 1.
+		"""Habilita al track durante la marca de tiempo en la cual \n
+		esta parada el cursor."""
+		editor.cursor.dato[track]="#"
 
 	def do_TRACKOFF(self, track):
-		"""Operación inversa del TRACKON."""
-		# Ejemplo :
-		# 	Suponiendo que se tiene 1 track:
-		# 	TRACKON 1
-		# 	TRACKOFF 1
-		# 	Cuando se reproduce esta marca, no se reproduce el sonido del track1.
+		"""Operacion inversa del TRACKON."""
+		editor.cursor.dato[track]="."
 
 	def do_PLAY(self):
 		"""Reproduce la marca en la que se encuentra el cursor actualmente."""
 	def do_PLAYALL(self):
-		"""Reproduce la canción completa desde el inicio."""
+		"""Reproduce la cancion completa desde el inicio."""
 	def do_PLAYMARKS (self, N):
-		"""Reproduce las próximas N marcas desde la posición actual del cursor."""
+		"""Reproduce las proximas N marcas desde la posicion actual del cursor."""
 		# Ejemplo :
 		# 	PLAYMARKS 5
-		# 	
+
 
 	def do_PLAYSECONDS (self, N):
-		"""Reproduce la canción completa desde el inicio.\n
-		Reproduce los próximos N segundos la posición actual del cursor.\n
-		Si alguna marca dura mas del tiempo restante, la reproducción \n
+		"""Reproduce la cancion completa desde el inicio.\n
+		Reproduce los proximos N segundos la posicion actual del cursor.\n
+		Si alguna marca dura mas del tiempo restante, la reproduccion \n
 		se corta antes."""
 		# Ejemplo :
 		# 	PLAYSECONDS 5
-		
+	def do_PRINT(self):
+		for mark in editor.timeline:
+			print("duracion",mark(0))
+			print(mark(1).get("."))
 Shell().cmdloop()
+
+
+
+
+
---
+++ tda.py
@@ -0,0 +1,173 @@
+
+class _Nodo:
+	'''Representacion de un nodo'''
+	def __init__(self, dato, prox=None, prev = None):
+		"""Constructor del nodo"""
+		self.dato = dato
+		self.prox = prox
+		self.prev = prev
+class ListaEnlazada:
+	"""Clase la lista enlazada"""
+	def __init__(self):
+		"""Constructor de la lista enlazada"""
+		self.prim = None
+		self.len = 0
+	def __iter__(self):
+		return _IteradorListaEnlazada(self.prim)
+	def __str__(self):
+		lista = []
+		for dato in self:
+			lista.append(dato)
+		return str(lista)
+	def append(self, dato):
+		"""Agrega el dato al final de la lista"""
+		self.insert(self.len, dato)
+	def insert(self, i, dato):
+		"""Inserta el dato recibido en la posicion indicada\n
+			->i (int): indice, no puede ser cero o mayor a la longitud de la lista\n
+			->dato(): valor a agregar"""
+		if i < 0 or i > self.len:
+			raise IndexError
+		nodo = _Nodo(dato)
+		self.len += 1
+		if i==0:
+			nodo.prox = self.prim
+			self.prim = nodo
+		else:
+			actual = self.prim
+			for j in range(1,i):
+				actual = actual.prox
+			nodo.prox = actual.prox
+			actual.prox = nodo
+	def extend(self, otra):
+		"""Conecta dos listas, manteniendo la segunda independiente"""
+		if otra.len==0:
+			return 
+		if self.prim is None:
+			self.prim=_Nodo(otra.prim.dato)
+			aux = otra.prim.prox
+		else:
+			aux = otra.prim
+		actual = self.prim
+		while actual.prox is not None:
+			actual = actual.prox
+		while aux is not None:
+			actual.prox = _Nodo (aux.dato)
+			actual = actual.prox
+			aux = aux.prox
+	def invert(self):
+		actual=self.prim
+		anterior=None
+		while actual is not None:
+			prox=actual.prox
+			actual.prox = anterior
+			anterior = actual
+			actual = prox
+		self.prim=anterior
+	def remove(self,elemento):
+		"""Quita de la lista el elemento en su primera aparicion, si se encuentra.\n
+		De no encontrarse en la lista,se produce un error. Si la lista esta vacia,\n
+		levanta un error."""
+		if self.len == 0:
+			raise ValueError ("La lista esta vacia")
+		if self.prim.dato == elemento:
+			self.prim = self.prim.prox
+			self.len -= 1
+		else:
+			actual = self.prim
+			while actual.prox is not None:
+				siguiente = actual.prox
+				if siguiente.dato == elemento:
+					actual.prox=siguiente.prox
+					self.len -= 1
+					return
+				actual = actual.prox
+		raise ValueError ("No se encontro tal elemento en la lista")
+	def pop(self,posicion = None):
+		"""Elimina el elemento en la posicion ingresada, y lo devuelve.\n
+			-> posicion (int): posicion a eliminar \n"""
+		if self.len == 0:
+			raise ValueError ("La lista esta vacia")
+		if posicion is None:
+			posicion = self.len-1
+		elif 0 > posicion > self.len+1:
+			raise IndexError ("Ingreso una posicion fuera de la lista")
+		self.len -= 1
+		actual = self.prim
+		if posicion == 0:
+			self.prim = actual.prox
+		for i in range (1,posicion):
+			actual = actual.prox
+		if posicion == self.len-1:
+			actual.prox = None
+		else: 
+			actual.prox=actual.prox.prox
+		return actual.dato
+class _IteradorListaEnlazada:
+	def __init__(self, posicion):
+		self.actual = posicion
+	def __next__(self):
+		if self.actual is None:
+			raise StopIteration()
+		dato = self.actual.dato
+		self.actual = self.actual.prev
+		return dato
+class Pila:
+	"""docstring for Pila"""
+	def __init__(self):
+		"""Constructor for Pila"""
+		self.elementos= []
+	def esta_vacia(self):
+		"""Devuelve un booleano segun la pila esta vacia o no."""
+		return len(self.elementos)==0
+	def apilar(self,dato):
+		"""Agrega un dato a la pila."""
+		self.elementos.append(dato)
+	def desapilar(self):
+		"""Elimina el ultimo elemento ingresado de la pila y lo devuelve."""
+		if self.esta_vacia():
+			raise ValueError ("La pila esta vacia")
+		return self.elementos.pop()
+	def tope(self):
+		"""Devuelve el ultimo elemento ingresado en la lista.\n
+		Error si esta vacia"""
+		if self.esta_vacia():
+			raise ValueError("La pila esta vacia")
+		return self.elementos[-1]
+	def invertir(self):
+		"""Devuelve la misma pila con sus elementos invertidos."""
+		cola_aux = Cola()
+		while not self.esta_vacia():
+			cola_aux.encolar(self.desapilar())
+		while not cola_aux.esta_vacia():
+			self.apilar(cola_aux.desencolar())
+class Cola:
+	"""docstring for Cola"""
+	def __init__(self, arg):
+		"""Constructor for Cola"""
+		self.primero=None
+		self.ultimo=None
+	def esta_vacia(self):
+		"""Devuelve un booleano segun la cola esta vacia o no."""
+		return self.primero is None
+	def encolar(self,dato):
+		"""Agrega un dato a la cola."""
+		nodo=_Nodo(dato)
+		if self.ultimo is not None:
+			self.ultimo.prox=nodo
+		else:
+			self.primero=nodo
+		self.ultimo=nodo
+	def desencolar(self):
+		"""Elimina el primer elemento de la cola y lo devuelve."""
+		if self.esta_vacia():
+			raise ValueError ("La cola no tiene primero, esta vacia.")
+		dato=self.primero.dato
+		self.primero=self.primero.prox
+		return dato
+	def ver_primero(self):
+		"""Devuelve el ultimo elemento ingresado en la lista.\n
+		Error si esta vacia."""
+		if self.esta_vacia():
+			raise ValueError ("La cola no tiene primero, esta vacia.")
+		return self.primero.dato
