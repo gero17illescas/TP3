97bf20292a17f7b898f40e4fce4b078dbfa82494
gero17illescas <gero17illescas@gmail.com>
2017-05-25 17:16:38 -0300

    STORE
    Desarrollo de la funcion STORE que almacena en un archivo la informacion. Graba todo el encabezado bien pero solo escribe el primer mark. Tengo problemas con la funcion TRACKON, al parecer algo sucede con editor.cursor que hace referencia a algun NoneType cuando deberia hacerlo a nodos del Timeline

--- modo_edicion.py
+++ modo_edicion.py
@@ -2,31 +2,43 @@ import cmd
 import soundPlayer
 import tda
 class Editor():
+	"""Representacion del editor, tiene las funciones de avanzar y \n
+	retroceder asi como los atributos en lo que se almacena la \n
+	informarcion de la cancion."""
 	def __init__(self):
+		"""Constructor de Editor"""
 		self.timeline = tda.ListaEnlazada()		#marks
 		self.cursor = self.timeline.prim		#hace referencia al mark actual
 		self.index = 0							#indica en que indice se encuntra el cursor
 		self.pila = tda.Pila()					#aux para retroceder
-		self.tracks = tda.ListaEnlazada()		#tracks o canales que tendra la cancion
-		self.sound = {"sine": soundPlayer.SoundFactory.get_sine_sound,
-			 "squa": soundPlayer.SoundFactory.get_square_sound,
-			 "sile": soundPlayer.SoundFactory.get_silence_sound,
-			 "tria": soundPlayer.SoundFactory.get_triangular_sound,
-			 "nois": soundPlayer.SoundFactory.get_noise_sound,}
+		self.tracks = []		#datos de los tracks o canales que tendra la cancion
+		self.sound = {
+			"sine": soundPlayer.SoundFactory.get_sine_sound,
+			"squa": soundPlayer.SoundFactory.get_square_sound,
+			"sile": soundPlayer.SoundFactory.get_silence_sound,
+			"tria": soundPlayer.SoundFactory.get_triangular_sound,
+			"nois": soundPlayer.SoundFactory.get_noise_sound,}
 	def avanzar(self, N):
-		self.index += N 
+		"""Avanza N nodos sobre donde se encuentra el cursor.\n
+		Si se llega al final de la lista deja de avanzar.\n
+			-> N (int): es la cantidad de pasos."""
+		self.index += N
 		cont = 0
 		actual = self.cursor #a partir de donde estamos
 		while cont < N and actual is not None:
 			actual = actual.prox
 			self.pila.apilar(actual)
-			cont += 1 
+			cont += 1
 		self.cursor = actual
 	def retroceder(self, N):
+		"""Avanza N nodos sobre donde se encuentra el cursor.\n
+		Si se llega al principio de la lista deja de retroceder.\n
+			-> N (int): es la cantidad de pasos."""
 		self.index -= N
-		if not self.pila.esta_vacia():
-			for _ in range (N):
-				self.cursor = self.pila.desapilar()
+		cont = 0
+		while cont < N and not self.pila.esta_vacia():
+			self.cursor = self.pila.desapilar()
+			cont += 1
 editor = Editor()
 class Shell(cmd.Cmd):
 	intro = "Bienvenido a mi programa.\n Ingrese help o ? para listar los comandos.\n"
@@ -39,10 +51,26 @@ class Shell(cmd.Cmd):
 		# editor queda al inicio de la misma. Si no existe el archivo, se le debe
 		# informar al usuario.
 	def do_STORE (self,file):
-		"""Guarda la cancion."""
-		# Ejemplo :
-		# 	STORE shadow world.plp
-		# 	Guarda la cancion en edicion actual con el nombre indicado.
+		"""Guarda la cancion.\n
+			-> file (str)"""
+		try:
+			with open(file,"w") as f:
+				f.write("FIELD,DATA\n")						#cabecera
+				f.write("C,{}\n".format(len(editor.tracks)))	#Escribimos la cantidad de canales
+				for i in range(len(editor.tracks)):			#Escribimos la info de los canales
+					(funcion, frecuencia, volumen) = editor.tracks[i]
+					f.write("C,{}|{}|{}\n".format(funcion, frecuencia, volumen))
+				duracion_anterior = 0
+				for mark in editor.timeline:
+					if mark["duracion"] != duracion_anterior:
+						duracion_anterior = mark.get("duracion")
+						f.write("T,{}\n".format(duracion_anterior))
+					cadena_mark = ""
+					for i in range(len(editor.tracks)):
+						cadena_mark += mark.get(i,".")
+					f.write(cadena_mark+"\n")
+		except IOError :
+			raise IOError ("Hubo un problema con el archivo")
 
 	def do_STEP(self):
 		"""Avanza a la siguiente marca de tiempo.Si no hay mas marcas \n
@@ -60,17 +88,17 @@ class Shell(cmd.Cmd):
 		"""Retrocede N marcas de tiempo hacia atras. Si no hay mas mar\n
 		cas hacia atras, no hace nada."""
 		editor.retroceder(int(N))
-	def do_TRACKADD (self, funcion, frecuencia=400, volumen=100):
+	def do_TRACKADD (self, funcion, frecuencia=440, volumen=100):
 		"""Agrega un track con el sonido indicado.\n
 			-> funcion (str): tipo de sonido\n
 			-> frecuencia(int)\n
 			-> volumen(int)\n"""
-		editor.tracks.append(editor.sound.get(funcion)(frecuencia,volumen))
+		editor.tracks.append((funcion, frecuencia, volumen))
 	def do_TRACKDEL(self, posicion=None):
 		"""Elimina un track por numero.Si no se especifica uno elimina\n
 		el ultimo tarck.\n
 			-> posicion(int)"""
-		if posicion<0 or posicion>editor.tracks.len:
+		if posicion<0 or posicion>len(editor.tracks):
 			raise IndexError ("La posicion ingresada no corresponde a un track ingresado")
 		editor.tracks.pop(posicion)
 	def do_MARKADD(self,duration):
@@ -88,6 +116,7 @@ class Shell(cmd.Cmd):
 	def do_TRACKON(self, track):
 		"""Habilita al track durante la marca de tiempo en la cual \n
 		esta parada el cursor."""
+		print(editor.cursor.dato)
 		editor.cursor.dato[track]="#"
 	def do_TRACKOFF(self, track):
 		"""Operacion inversa del TRACKON."""
@@ -105,30 +134,28 @@ class Shell(cmd.Cmd):
 	def do_PLAYMARKS (self, N):
 		"""Reproduce las proximas N marcas desde la posicion actual del\n
 		 cursor."""
-		reproductor = soundPlayer.SoundPlayer(editor.tracks.len)
+		if editor.timeline.len == 0:
+			raise ValueError ("No hay marcas para reproducir")
+		reproductor = soundPlayer.SoundPlay(len(editor.tracks))
 		actual = editor.cursor
 		while actual is not None:
 			lista_aux = []
+			lista_tracks = []
 			mark = actual.dato
 			duracion = mark.pop("duracion")
-			for i in range(editor.tracks.len):
+			for i in range(len(editor.tracks)):
+				(funcion, frecuencia, volumen) = editor.tracks[i]
 				if mark[i]=="#":
-					lista_aux.append(editor.tracks[i])
+					lista_aux.append(editor.sound.get(funcion)(frecuencia, volumen))
 			reproductor.play_sounds(lista_aux,duracion)
 			mark["duracion"]=duracion
 	def do_PLAYSECONDS (self, N):
-		"""Reproduce la cancion completa desde el inicio.\n
-		Reproduce los proximos N segundos desde la posicion actual del cursor.\n
-		Si alguna marca dura mas del tiempo restante, la reproduccion \n
-		se corta antes."""
+		"""Reproduce los proximos N segundos desde la posicion actual \n
+		del cursor. Si alguna marca dura mas del tiempo restante, la \n 
+		reproduccion se corta antes."""
 		# Ejemplo :
 		# 	PLAYSECONDS 5
 	def do_PRINT(self):
 		for mark in editor.timeline:
 			print(mark)
 Shell().cmdloop()
-
-
-
-
-
