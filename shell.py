19a647210a085a05915352a9fb859758c8c533eb
gero17illescas <gero17illescas@gmail.com>
2017-06-23 17:15:19 -0300

    Correcciones Shell
    Importar _Mark desde almacenamiento
    Correcion de funciones Trackon y Trackoff

--- shell.py
+++ shell.py
@@ -2,40 +2,35 @@ import cmd, sys
 import editor as e
 import reproductor as r
 import almacenamiento as a
-MENSAJE=(" Bienvenido a mi FIUBA Music editor.\n"+
-" Antes de activar los tracks debe crear al menos una marca.\n"+
-" Cuando carga un archivo elimina todo lo que este en memoria.\n"+
-" Ingrese help o ? para listar los comandos.\n")
-def es_entero(n):
-	"""Recibe y devuelve un booleano si es \n
-		->n(int ó str): es el valor validarse como entero"""
-	if type(n) == int and n >= 0:
+def es_numero(n):
+	"""Recibe y devuelve un booleano si es flotante o entero\n
+		->n(str):"""
+	try:
+		float(n)
 		return True
-	return n.isdigit() and int(n) >= 0
+	except ValueError:
+		return False
+
 def validar_parametro(argumento, max=None):
 	"""Recibe un argumento y si cumple las condiciones lo devulve (int)\n
 	Caso contrario devulve None.\n
 		->argumento(str)\n
 		->max(int): opcional\n"""
-	if es_entero(argumento):
-		argumento = int(argumento)
-		if max is not None:
-			if argumento < max:
-				return argumento
-			else:
-				print("Se ingreso un numero mayor que ",max)
-				return None
-		else:
-			return argumento
+	if es_numero(argumento):
+		argumento = float(argumento)
+		if max:
+			if argumento >= max:
+				print("Ingrese un numero menor que ",max)
+				return False
+		return True
 	else:
 		print("Lo ingresado no es un numero")
-		return None
-class _Mark():
-	"""Representacion de una marca."""
-	def __init__(self, duracion):
-		"""Constructor"""
-		self.duracion = duracion
-		self.tracks = {}
+		return False
+
+MENSAJE=(" Bienvenido a mi FIUBA Music editor.\n"+
+" Antes de activar los tracks debe crear al menos una marca.\n"+
+" Cuando carga un archivo elimina todo lo que este en memoria.\n"+
+" Ingrese help o ? para listar los comandos.\n")
 class Shell(cmd.Cmd):
 	intro = MENSAJE
 	prompt = "*>>"
@@ -70,16 +65,16 @@ class Shell(cmd.Cmd):
 	def do_avanzarm (self, n):
 		"""Avanza N marcas de tiempo hacia adelante. Si no hay mas mar\n
 		cas hacia adelante, no hace nada."""
-		n=validar_parametro(n) 
-		if n:
+		if validar_parametro(n):
+			n = int(n)
 			self.editor.avanzar(n)
 			print("Marca: ",self.editor.index)
 
 	def do_retrocederm (self, n):
 		"""Retrocede N marcas de tiempo hacia atras. Si no hay mas mar\n
 		cas hacia atras, no hace nada."""
-		n=validar_parametro(n) 
-		if n:
+		if validar_parametro(n):
+			n = int(n)
 			self.editor.retroceder(n)
 			print("Marca: ",self.editor.index)
 
@@ -91,9 +86,10 @@ class Shell(cmd.Cmd):
 			funcion, frecuencia, volumen= lista_p[0], lista_p[1], lista_p[2]
 		else:
 			funcion, frecuencia = lista_p[0], lista_p[1]
-			volumen = 100
-		frecuencia = validar_parametro(frecuencia)
-		volumen = validar_parametro(volumen)
+			volumen = 1
+		if validar_parametro(volumen) and validar_parametro(frecuencia):
+			frecuencia = float(frecuencia)
+			volumen = int(volumen)/100
 		if funcion in self.editor.sound:
 			self.editor.tracks.append((funcion, frecuencia, volumen))
 			return
@@ -102,50 +98,58 @@ class Shell(cmd.Cmd):
 	def do_trackdel(self, posicion=None):
 		"""Elimina un track por numero.Si no se especifica uno elimina\n
 		el ultimo tarck.\n
-			-> posicion(str)"""		
-		if posicion:
-			posicion=validar_parametro(posicion)
-			if posicion is not None:
-				self.editor.tracks.pop(posicion)
-				print("Marca: ",self.editor.index)
-		else:
+			-> posicion(str)"""	
+		if posicion is None: #Aca si o si tuve que usar el is none
+							 #Si usaba un ==, auqnue posicion no fuera un 
+							 #None igual cumplia el if(aunque no quisieramos)
 			self.editor.tracks.pop(len(self.editor.tracks) - 1)
-			
+			return
+		if validar_parametro(posicion,len(self.editor.tracks)):
+			posicion = int(posicion)
+			self.editor.tracks.pop(posicion)
+			print("Marca: ",self.editor.index)
+
 	def do_markadd(self, duration):
 		"""Agrega una marca de tiempo de la duracion establecida.\n
 		Originalmente todos los tracks arrancan como deshabilitados.\n
 		Siempre que se agrega una marca el cursor vuelve al inicio."""
-		duration = validar_parametro(duration) 
-		if duration:
-			self.editor.timeline.append( _Mark(duration/100))
-			self.editor.cursor = self.editor.timeline.prim
-
+		if validar_parametro(duration):
+			duration = float(duration)
+			self.editor.timeline.append( a._Mark(duration/100))
+			if self.editor.index == 0:
+				self.editor.cursor = self.editor.timeline.prim
 	def do_markaddnext(self, duration):
 		"""Igual que MARKADD pero la inserta luego de la marca en la \n
 		cual esta actualmente el cursor."""
-		duration = validar_parametro(duration) 
-		if duration:
-			self.editor.timeline.insert(self.editor.index+1,_Mark(duration/100))
+		if validar_parametro(duration):
+			duration = float(duration)
+			self.editor.timeline.insert(self.editor.index+1, a._Mark(duration/100))
 
 	def do_markaddprev(self,duration):
 		"""Igual que MARKADD pero la inserta antes de la marca en la \n
 		cual esta actualmente el cursor."""
-		duration = validar_parametro(duration) 
-		if duration:
-			self.editor.timeline.insert(self.editor.index,_Mark(duration/100))
+		if validar_parametro(duration):
+			duration = float(duration)
+			self.editor.timeline.insert(self.editor.index, a._Mark(duration/100))
 
 	def do_trackon(self, indice):
 		"""Habilita al track durante la marca de tiempo en la cual \n
 		esta parada el cursor."""
-		indice = validar_parametro(indice,len(self.editor.tracks)) 
-		if indice is not None:
-			self.editor.cursor.dato.tracks[self.editor.tracks[indice]] = "#"
+		if validar_parametro(indice,len(self.editor.tracks)):
+			indice = int(indice)
+			if self.editor.timeline.len > 0:
+				self.editor.cursor.dato.tracks[self.editor.tracks[indice]] = "#"
+			else:
+				print("No hay marcas.Agregue una antes de activar un track")
 
 	def do_trackoff(self, indice):
 		"""Operacion inversa del TRACKON."""
-		duration = validar_parametro(indice,len(self.editor.tracks)) 
-		if duration is not None:
-			self.editor.cursor.dato.tracks.pop(self.editor.tracks[indice])
+		if validar_parametro(indice,len(self.editor.tracks)):
+			indice = int(indice)
+			if self.editor.timeline.len > 0:
+				self.editor.cursor.dato.tracks.pop(self.editor.tracks[indice],False)
+			else:
+				print("No hay marcas.Agregue una antes de activar un track")
 
 	def do_play(self, x = None):
 		"""Reproduce la marca en la que se encuentra el cursor actual\n
@@ -162,31 +166,31 @@ class Shell(cmd.Cmd):
 	def do_playmarks (self, n):
 		"""Reproduce las proximas N marcas desde la posicion actual del\n
 		 cursor."""
-		n = validar_parametro(n)
-		if n:
+		if validar_parametro(n):
+			n = int(n)
 			r.Reproductor(self.editor).play(n)
 		
 	def do_playsecond(self, n):
 		"""Reproduce los proximos N segundos desde la posicion actual \n
 		del cursor. Si alguna marca dura mas del tiempo restante, la \n
 		reproduccion se corta antes."""
-		n = validar_parametro(n)
-		if n:
-			r.Reproductor(self.editor)(self.editor.timeline.len,n)
+		if validar_parametro(n):
+			n = int(n)
+			r.Reproductor(self.editor).play(self.editor.timeline.len, n)
 
 	def do_imprimir(self, x = None):
 		"""Imprime la timeline que se tiene hasta el momento"""
-		for (funcion, frecuencia, volumen) in self.editor.tracks:
-			print("Funcion: {} Frecuencia: {} Volumen: {}".format(funcion, frecuencia, volumen))
-		cont = 0
-		for mark in self.editor.timeline:
-			cadena = "["+str(mark.duracion)+"]: "
+		for i,(funcion, frecuencia, volumen) in enumerate(self.editor.tracks):
+			print("{} Funcion: {} Frecuencia: {} Volumen: {}".format(i,funcion, frecuencia, volumen*100))
+		print("N marca\tduracion  trackon")
+		for i,mark in enumerate(self.editor.timeline):
+			cadena = "{}\t[{}+]\t: ".format(i,mark.duration)
 			for track in self.editor.tracks:
 				cadena += mark.tracks.get(track,".")
-			if self.editor.index == cont:
+			if self.editor.index == i:
 				cadena += "<-"	#Señala el cursor
-			cont += 1
 			print(cadena)
+			
 	def do_salir(self, x = None):
 		"""Sale del programa"""
 		print("Hasta luego")
